bc1acf83088dec2a30afbdc1669ca6c5f5fed645
Norbert Dzikowski <norbert.dzikowski@yahoo.de>
2023-12-04 03:36:56 +0100

    test: Extend rudimentary test suite

--- test/main.c
+++ test/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+typedef int (*TEST_CASE)(void);
+typedef enum {
+	FAILURE,
+	SUCCESS,
+} TEST_RESULT;
+
+int some_test(void) {
+	printf("hello_from_test\n");
+	return SUCCESS;
+}
+
 int main() {
-	printf("Hello, World!\n");
+	TEST_CASE test_cases[] = {
+		some_test,
+		NULL, // Sentinel value, always last element.
+	};
+	TEST_CASE test_case = test_cases[0];
+	printf("unknown-test-running...\n");
+	for (int o = 1; test_case != NULL; ++o) {
+		if(test_case()) {
+			printf("test-case true\n");
+		}
+		test_case = test_cases[o];
+	}
+	printf("unknown-test-finished...\n");
 	return 0;
 }
